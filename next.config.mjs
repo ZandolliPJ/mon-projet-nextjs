995b328c0c14a14be611b84334030da08428ff47
ZandolliPJ <judithpascal07@gmail.com>
2025-02-04 20:04:14 +0100

    gestion test2 fichier netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
+// next.config.js
+const withNetlify = require('next-on-netlify');
+module.exports = withNetlify();
 export default nextConfig;
